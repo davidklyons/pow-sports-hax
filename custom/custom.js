13e6976ba26d125574c80efe91172092f6a323a3
davidklyons <davidklyons@outlook.com>
2025-12-15 19:12:11 -0500

    'Page details updated: Home (item-7dbdc902-9652-4e80-8aa4-bcb293f4a409)'

---
+++ custom/custom.js
@@ -0,0 +1,24 @@
+// MENU API
+fetch(`${document.baseURI}api/menu.json`)
+  .then(res => res.json())
+  .then(data => {
+    console.log("MENU DATA:", data);
+  });
+
+// SCHEDULE API
+fetch(`${document.baseURI}api/schedule.json`)
+  .then(res => res.json())
+  .then(data => {
+    console.log("SCHEDULE DATA:", data);
+  });
+
+// COMPONENT IMPORTS
+import "./psyba-hero-banner.js";
+import "./psyba-announcement-band.js";
+import "./psyba-about-band.js";
+import "./psyba-stats-band.js";
+import "./psyba-news-band.js";
+import "./psyba-site-footer.js";
+import "./psyba-schedule-band.js";
+
+console.log("PSYBA custom components registered");
